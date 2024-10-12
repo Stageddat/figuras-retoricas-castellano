02390bb79b78cf10147db12993c228624b6d792d
Stageddat <chmarc72@gmail.com>
2024-10-12 19:49:00 +0200

    feat: add recursos lexico semantico

--- src/estudiar/estudiar.tsx
+++ src/estudiar/estudiar.tsx
@@ -1,12 +1,14 @@
 import './estudiar.css'
 import { LoadRfonicos } from './loadRfonicos';
 import { LoadRsintacticos } from './loadRsintacticos';
+import { LoadRLexicoSemantico } from './loadRlexicosemantico';
 
 function EstudiarContent() {
     return (
         <div className='recursosContainer'>
             <LoadRfonicos/>
             <LoadRsintacticos/>
+            <LoadRLexicoSemantico/>
             </div>
     );
 }
---
+++ src/estudiar/loadRlexicosemantico.tsx
@@ -0,0 +1,18 @@
+import rlexicosemantico from '../../db/rlexicosemantico.json'
+
+export function LoadRLexicoSemantico() {
+    const entries = Object.entries(rlexicosemantico);
+
+    return (
+        <div className='parentContainer'>
+            <h3 className='recursoCategoryHeader'>Recursos léxico-semánticos</h3>
+            {entries.map(([key, value], index) => (
+                <div key={index} className='recursoItem'>
+                    <div className='recursoHeader'>{key}</div>
+                    <div className='recursoDefinicion'>{value.Definicion}</div>
+                    <div className='recursoEjemplo'>{value.Ejemplo}</div>
+                </div>
+            ))}
+        </div>
+    );
+}
