a50a106d978f12bc81866de745c273e16130dbee
Stageddat <chmarc72@gmail.com>
2024-10-20 21:56:41 +0200

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: "/recursos-expresivos-castellano/",
+    // base: "/recursos-expresivos-castellano/",
+    base: "/",
     plugins: [react()],
     build: {
         outDir: 'dist',
