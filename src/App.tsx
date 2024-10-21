04cff5708aa8a949e75ca8da3b96fe12896d049b
Stageddat <chmarc72@gmail.com>
2024-10-21 20:37:16 +0200

    feat: add dynamic basename

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-// App.tsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import EstudiarContent from './components/estudiarContent';
 import './style.css';
 import MainContent from './components/mainContent';
+
+const base = process.env.NODE_ENV === 'production' ? '/recursos-expresivos-castellano' : '';
+
 function App() {
     return (
-        <Router>
+        <Router basename={base}>
             <div>
                 <h1 className="title">FIGURAS RETÓRICAS</h1>
 
                 <Routes>
-                    <Route
-                        path="/"
-                        element={<MainContent />}
-                    />
+                    <Route path="/" element={<MainContent />} />
                     <Route path="/estudiar" element={<EstudiarContent />} />
                     {/* <Route path="/practicar" element={<Practicar />} /> */}
                 </Routes>
