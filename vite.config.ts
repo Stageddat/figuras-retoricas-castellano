d8c10255ef0fa6f4e99861247cec72aab056dbe2
Stageddat <chmarc72@gmail.com>
2024-10-21 20:34:07 +0200

    feat: add variable in deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: "/recursos-expresivos-castellano/",
-    // base: "/",
+    base: process.env.NODE_ENV === 'production' ? '/recursos-expresivos-castellano/' : '/',
     plugins: [react()],
     build: {
         outDir: 'dist',
