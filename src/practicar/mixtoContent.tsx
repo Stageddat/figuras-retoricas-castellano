498e5aa084592c5b43ea674eda8a255cb3c622b4
Stageddat <chmarc72@gmail.com>
2025-02-06 23:16:12 +0100

    feat: add MixtoContent component to integrate FigurasRetoricas

---
+++ src/practicar/mixtoContent.tsx
@@ -0,0 +1,13 @@
+import { FigurasRetoricas } from "../estudiar/loadFiguras";
+
+function MixtoContent() {
+    console.log("hai")
+    return (
+        <>
+            <FigurasRetoricas />
+        </>
+
+    );
+}
+
+export default MixtoContent;
