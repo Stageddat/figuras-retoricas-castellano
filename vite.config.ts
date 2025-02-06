bf699553c72c2157744269e3e0aa274025612699
Stageddat <chmarc72@gmail.com>
2025-02-06 23:36:13 +0100

    fix: remove base

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: process.env.NODE_ENV === 'production' ? '/recursos-expresivos-castellano/' : '/',
     plugins: [react()],
     build: {
         outDir: 'dist',
