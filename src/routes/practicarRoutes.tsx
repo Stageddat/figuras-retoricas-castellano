af452a1c25327f94fe9f069382e769449ee7c64e
Stageddat <chmarc72@gmail.com>
2025-02-08 01:06:47 +0100

    feat: add DefinicionQuiz component and update routing

--- src/routes/practicarRoutes.tsx
+++ src/routes/practicarRoutes.tsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import PracticarMainMenuContent from "../components/practicarMainMenuContent";
 import EjemploQuiz from "../practicar/ejemploQuiz";
+import DefinicionQuiz from "../practicar/definicionQuiz";
 function PracticarMainMenuRoutes() {
     return (
         <Routes>
             <Route path="/" element={<PracticarMainMenuContent />} />
             <Route path="/mixto" element={<>mixto xd</>} />
             <Route path="/nombre" element={<>nombre xd</>} />
-            <Route path="/significado" element={<>significado xd</>} />
+            <Route path="/significado" element={<DefinicionQuiz />} />
             <Route path="/ejemplo" element={<EjemploQuiz />} />
         </Routes>
     );
